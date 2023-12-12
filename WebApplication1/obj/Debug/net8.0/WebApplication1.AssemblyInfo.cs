1334ecfb3be881b1f69122b3e4a6b97e4f06a848
Vance <haibiev.amir@gmail.com>
2023-12-12 12:32:31 +0300

    Add PostsController

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329bd53cf5b0657febd74460813bc399d673e9ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb23a6e2dba57962ab8d3b5c91db09bc2521d594")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
