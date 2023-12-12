329bd53cf5b0657febd74460813bc399d673e9ae
Vance <haibiev.amir@gmail.com>
2023-12-12 12:04:22 +0300

    Soma changes

--- WebApplication1/Models/AppDbContext.cs
+++ WebApplication1/Models/AppDbContext.cs
@@ -4,7 +4,6 @@ namespace WebApplication1.Models
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
