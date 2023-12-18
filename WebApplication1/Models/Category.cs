d96cb405e63e898b139ec07bbb2266e70dba66db
Vance <haibiev.amir@gmail.com>
2023-12-18 15:31:42 +0300

    Add Cats

---
+++ WebApplication1/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class Category
+{
+    //[DatabaseGenerated(DatabaseGeneratedOption.None)]
+    //[Key]
+    [Display(Name="ID")]
+    public int Id { get; set; }
+    [Display(Name="Название")]
+    public string? Name { get; set; }
+    [Display(Name="Посты")]
+    public ICollection<Post> Posts { get; set; } = new List<Post>();
+}
